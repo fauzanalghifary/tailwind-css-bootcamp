a70be7f7fba832caf9a4618cc9bc8ead187160fe
fauzanalghifary <m.fauzanghifary@gmail.com>
2023-02-19 17:40:33 +0700

    finish course

---
+++ tailwind-project-6-bookmark-manager/js/script.js
@@ -0,0 +1,48 @@
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
+
+// Tabs menu event listener
+tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+// Hamburger button listener
+btn.addEventListener("click", navToggle);
+
+function onTabClick(e) {
+  // Deactivate all tabs
+  tabs.forEach((tab) => {
+    tab.children[0].classList.remove("border-softRed", "border-b-4");
+  });
+
+  // Hide all panels
+  panels.forEach((panel) => {
+    panel.classList.add("hidden");
+  });
+
+  const classString = e.target.getAttribute("data-target");
+  // Activate a new tab and panel
+  tabs.forEach((tab) => {
+    if (tab.getAttribute("data-target") === classString) {
+      tab.children[0].classList.add("border-softRed", "border-b-4");
+    }
+  });
+
+  document
+    .getElementById("panels")
+    .getElementsByClassName(classString)[0]
+    .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+
+  if (menu.classList.contains("flex")) {
+    logo.setAttribute("src", "./images/logo-bookmark-footer.svg");
+  } else {
+    logo.setAttribute("src", "./images/logo-bookmark.svg");
+  }
+}
